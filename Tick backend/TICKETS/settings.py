393cbc19784b104b44db00f9e1541d00182ad5c5
amani-bousselidj <amani.bousselidj@univ-constantine2.dz>
2024-01-29 13:01:25 +0100

    version backend

--- Tick backend/TICKETS/settings.py
+++ Tick backend/TICKETS/settings.py
@@ -59,14 +59,12 @@ MIDDLEWARE = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost",
-    "http://localhost:5173",
+    "http://localhost:5173/",
     'http://127.0.0.1',
-    # "https://aml-school.com"  # For production
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    # "https://aml-school.com",
-    "http://localhost",
+    "http://localhost:5173/",
 ]
 ROOT_URLCONF = 'TICKETS.urls'
 
