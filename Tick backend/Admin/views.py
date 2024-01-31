c75e53c4e97e3955e72f476e746b28ceb4a5056a
amani-bousselidj <amani.bousselidj@univ-constantine2.dz>
2024-01-31 16:46:42 +0100

    up date backend profile

--- Tick backend/Admin/views.py
+++ Tick backend/Admin/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from Events.models import Event
 from .serializers import *
 from .permissions import IsStaffUser
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
@@ -32,3 +32,29 @@ class TheaterListView(generics.ListAPIView):
 class TheaterRetrieveDestroyView(generics.RetrieveDestroyAPIView):
     queryset = Theater.objects.all()
     serializer_class = TheaterSerializer
+
+
+class UserslistView(generics.ListAPIView):
+    queryset= CustomUser.objects.all()
+    serializer_class = UserSerializer 
+
+class UserRetreivedestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+from rest_framework.response import Response
+class TicketUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketupdateSerilizer
+
+    def perform_update(self, serializer):
+        instance = serializer.instance
+        if 'status' in serializer.validated_data:
+            status_value = serializer.validated_data['status']
+            if status_value == 'Refused':
+                instance.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+        serializer.save()
+class TciketListview(generics.ListAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerilizer
