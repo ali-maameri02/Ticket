5fbd547e518b47fc9bf5fb59a0d2accfd324e0cd
ali-maameri02 <ali.maameri@univ-constantine2.dz>
2024-02-01 12:09:56 +0100

    link admin login

--- Tick backend/Admin/views.py
+++ Tick backend/Admin/views.py
@@ -3,6 +3,12 @@ from Events.models import Event
 from .serializers import *
 from .permissions import IsStaffUser
 from rest_framework import viewsets,status
+from rest_framework.views import APIView
+from Accounts.serializers import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token  # Update this import
+
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
@@ -58,3 +64,30 @@ class TicketUpdateView(generics.RetrieveUpdateAPIView):
 class TciketListview(generics.ListAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerilizer
+
+
+class AdminLoginView(APIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+
+        user = None
+
+        if '@' in username:
+            try:
+                user = CustomUser.objects.get(email=username)
+            except ObjectDoesNotExist:
+                pass
+
+        if not user:
+            user = authenticate(username=username, password=password)
+
+        if user and user.is_staff:  # Check if the user is staff
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'user_id': user.id, 'username': user.username}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials or user is not staff'}, status=status.HTTP_401_UNAUTHORIZED)
